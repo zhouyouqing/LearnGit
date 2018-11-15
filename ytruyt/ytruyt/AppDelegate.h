bd284b47abe18fae79b7d8302c542e6f20644e3c
zouzhou <474478823@qq.com>
2018-11-15 18:21:12 +0800

    first commit

---
+++ ytruyt/ytruyt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ytruyt
+//
+//  Created by apple on 2018/11/15.
+//  Copyright © 2018 gogokuaiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ytruyt/ytruyt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ytruyt
+//
+//  Created by apple on 2018/11/15.
+//  Copyright © 2018 gogokuaiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
